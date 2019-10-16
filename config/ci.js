6eb0b35ff184b31b46d6c5ced5c9313282e3317d
José Esquivel <joseluis_ev@hotmail.com>
2019-10-16 13:18:55 -0500

    initial commit

--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://jluisev:password1@ds333238.mlab.com:33238/blog_dev',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
   cookieKey: '123123123',
   redisUrl: 'redis://127.0.0.1:6379'
 };
